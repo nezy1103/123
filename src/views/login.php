d165d59a4c0bf8daf1ce42bfb256c2ab2e624661
nezy1103 <nezy1103@gmail.com>
2026-06-07 17:28:53 +0300

    Update login.php

--- src/views/login.php
+++ src/views/login.php
@@ -1,3 +1,8 @@
-<!DOCTYPE html><body><h2>Вход</h2><?php if(isset($e))echo"<p style=red>$e</p>";?>
-<form method=POST><input name=email placeholder=Email required><br><input name=password type=password placeholder=Пароль required><br><button>Войти</button></form>
-<a href=/auth/register>Регистрация</a></body></html>
+<h2>Вход</h2>
+<form method="POST">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Пароль" required><br>
+    <button type="submit">Войти</button>
+    <?php if (isset($error)) echo "<p>$error</p>"; ?>
+</form>
+<a href="/auth/register">Регистрация</a>
