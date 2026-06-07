53ddc7d5d4b6df5eb98f23e16b2d4660ac83101e
nezy1103 <nezy1103@gmail.com>
2026-06-07 17:16:54 +0300

    Update User.php

--- src/models/User.php
+++ src/models/User.php
@@ -1,9 +1,6 @@
 <?php
-require_once __DIR__.'/../config/db.php';
-
 class User {
     private $db;
-    
     public function __construct() {
         $this->db = Database::getInstance()->getConnection();
     }
@@ -12,7 +9,6 @@ class User {
         $stmt = $this->db->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->execute([$email]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        
         if ($user && password_verify($password, $user['password'])) {
             return $user;
         }
